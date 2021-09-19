ca5e6ab6fd7005cfef7ff5213be7c440a1a3ba1d
heojinho <hjh701234@gmail.com>
2021-09-19 14:02:14 +0900

    프로젝트 세팅

---
+++ src/main/java/com/example/jpaentityproject/JpaEntityProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpaentityproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaEntityProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaEntityProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jpaentityproject/JpaEntityProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpaentityproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaEntityProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
