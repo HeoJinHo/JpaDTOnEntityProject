73d716dc39044b8b6bcdd91d2562816a89797c61
HeoJinHo <hjh701234@gmail.com>
2023-03-03 12:14:50 +0900

    test1, 2

--- src/main/java/com/example/jpaentityproject/JpaEntityProjectApplication.java
+++ src/main/java/com/example/jpaentityproject/JpaEntityProjectApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JpaEntityProjectApplication {
 
     public static void main(String[] args) {
+        System.out.println("test1");
         SpringApplication.run(JpaEntityProjectApplication.class, args);
     }
 
